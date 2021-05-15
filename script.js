a7f8eb5c2330cabd6ff1e8cc89b26d1647eafafc
Benji <bwolfenberger@gmail.com>
2021-05-15 11:40:45 -0700

    added psuedocoding and initial asteroid object

---
+++ script.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    document.getElementByClass('moveBox').style.objectPosition = '0 10%';
+
+    // Create Asteroids
+    // Add movement
+    // Add ability to remove when clicked
+    
+
+    // Create Planet
+    // Add health attribute
+
+    // Create colision detection
+    // Decrease health upon colision
+
+
+
+
+
+
+
+
+
+
+})
