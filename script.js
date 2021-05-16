ada97f24b4715eb410a6d4f639ca415b6908df05
Benji <bwolfenberger@gmail.com>
2021-05-15 19:49:00 -0700

    moving object!

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    document.getElementByClass('moveBox').style.objectPosition = '0 10%';
+    let canvas = document.getElementById('myCanvas')
+    let asteroid = canvas.getContext('2d')
+    
+    let x = canvas.width/2
+    let y = canvas.height-30
+    let dx = 2
+    let dy = -2
+
+    function moveAsteroid() {
+        asteroid.clearRect(0, 0, canvas.width, canvas.height)
+        asteroid.beginPath()
+        asteroid.fillRect(x, y, 30, 30)
+        asteroid.fill()
+        asteroid.closePath()
+        x += dx
+        y += dy
+    }
+
+    setInterval(moveAsteroid, 10)
+
+
+
 
     // Create Asteroids
     // Add movement
     // Add ability to remove when clicked
-    
+
 
     // Create Planet
     // Add health attribute
@@ -14,12 +35,11 @@ window.addEventListener('DOMContentLoaded', () => {
     // Decrease health upon colision
 
 
-
-
-
-
-
-
-
-
 })
+
+
+
+
+
+
+
