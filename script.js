1616169afcff3efe8a5c888ec590c8fe64860dc6
Benji <bwolfenberger@gmail.com>
2021-05-16 17:11:02 -0700

    planet object created

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     let canvas = document.getElementById('myCanvas')
-    let asteroid = canvas.getContext('2d')
+    let ctx = canvas.getContext('2d')
     
     let x = 0
     let y = Math.floor(Math.random() * 600)
     let dx = 2
     let dy = -2
 
-    function moveAsteroid() {
-        asteroid.clearRect(0, 0, canvas.width, canvas.height)
-        asteroid.beginPath()
-        asteroid.fillRect(x, y, 30, 30)
-        asteroid.fill()
-        asteroid.closePath()
-        x += dx
-        y += dy
-        console.log(`x= ${x}`)
-        console.log(y)
+    let planetRadius = 40
+    let planetX = 600
+    let planetY = 300
+
+    function drawPlanet () {
+        ctx.beginPath()
+        ctx.arc(planetX, planetY, planetRadius, 0, Math.PI*2);
+        ctx.fillStyle = 'green'
+        ctx.fill()
+        ctx.closePath
+    }
+
+    
+
+    
+
+    function drawAsteroid() {
+        ctx.beginPath()
+        ctx.fillStyle = 'red'
+        ctx.fillRect(x, y, 30, 30)
+        ctx.fill()
+        ctx.closePath()
         if (x > 1300 || x < -100 || y > 700 || y < -100) {
             y = Math.floor(Math.random() * 600)
             x = 0
-            dx = Math.floor(Math.random() * 5)
-            dy = Math.floor(Math.random() * -2)
+            dx = Math.ceil(Math.random() * 5)
+            console.log(dx)
         }
-        
     }
 
+    function move () {
+        ctx.clearRect(0, 0, canvas.width, canvas.height)
+        drawAsteroid()
+        drawPlanet()
+        x += dx
+        y += dy
+    }
 
-    setInterval(moveAsteroid, 10)
+    
+
+
+    setInterval(move, 10)
 
 
 
 
-    // Create Asteroids
+    // Create ctxs
     // Add movement
     // Add ability to remove when clicked
 
