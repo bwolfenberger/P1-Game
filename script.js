d095325715c536a38737682bbccac031564e96a7
Benji <bwolfenberger@gmail.com>
2021-05-18 10:35:57 -0700

    circle detection added

--- script.js
+++ script.js
@@ -18,9 +18,20 @@ window.addEventListener('DOMContentLoaded', () => {
     let dx2 = 2
     let dy2 = -2
 
-    let planetTarget = {radius: 85, x: 600, y: 250}
+    // circle colllision detection
+    let planetTarget = {radius: 80, x: 600, y: 250}
     let asteroidTarget = {radius: 20, x: x, y: y}
+    let distanceX = planetTarget.x - asteroidTarget.x
+    let distanceY = planetTarget.y - asteroidTarget.y
+    let distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY)
+    
     let asteroid2Target = {radius: 17, x: x2, y: y2}
+    let distance2X = planetTarget.x - asteroid2Target.x
+    let distance2Y = planetTarget.y - asteroid2Target.y
+    let distance2 = Math.sqrt(distance2X * distance2X + distance2Y * distance2Y)
+
+
+
     let planetX = 500
     let planetY = 150
 
@@ -36,14 +47,24 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     function collisionDetection() {
-        if ((planetX + 20) > (x) && (planetX - 20) < (x + 30) && (planetY + 20) > (y) && (planetY - 20) < (y + 30)) {
+        planetTarget = {radius: 80, x: 600, y: 250}
+        asteroidTarget = {radius: 20, x: x, y: y}
+        distanceX = planetTarget.x - asteroidTarget.x
+        distanceY = planetTarget.y - asteroidTarget.y
+        distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY)
+        if (distance < planetTarget.radius + asteroidTarget.radius) {
             y = Math.floor(Math.random() * 600)
             x = -100
             dx = 4
             dy = Math.ceil(Math.random() * 4) - 2
             health = health - 25
         }
-        if ((planetX + 20) > (x2) && (planetX - 20) < (x2 + 30) && (planetY + 20) > (y2) && (planetY - 20) < (y2 + 30)) {
+
+        asteroid2Target = {radius: 17, x: x2, y: y2}
+        distance2X = planetTarget.x - asteroid2Target.x
+        distance2Y = planetTarget.y - asteroid2Target.y
+        distance2 = Math.sqrt(distance2X * distance2X + distance2Y * distance2Y)
+        if (distance2 < planetTarget.radius + asteroid2Target.radius) {
             y2 = Math.floor(Math.random() * 600)
             x2 = -100
             dx2 = 4
@@ -132,7 +153,7 @@ window.addEventListener('DOMContentLoaded', () => {
             checkClick(canvas, e);
         });
 
-    let interval = setInterval(move, 50)
+    let interval = setInterval(move, 5)
 
 
 
